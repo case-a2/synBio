7f8a780e593bb16822bc2918011640421ef45f54
Case Ashton <caseashton2@gmail.com>
2025-06-24 13:33:59 -0500

    Refactored URDF for scene

--- workspace_test/launch/display.launch.py
+++ workspace_test/launch/display.launch.py
@@ -1,7 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, PathJoinSubstitution, LaunchConfiguration
-
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
@@ -12,7 +11,7 @@ def generate_launch_description():
     ur_type = LaunchConfiguration("ur_type")
     description_package = FindPackageShare("workspace_test")
     description_file = PathJoinSubstitution(
-        [description_package, "urdf", "synbio_description.urdf.xacro"]
+        [description_package, "urdf", "synbio_scene.urdf.xacro"]
     )
 
     rvizconfig_file = PathJoinSubstitution([description_package, "rviz", "workspace_analysis.rviz"])
@@ -21,11 +20,34 @@ def generate_launch_description():
         Command(["xacro ", description_file, " ", "ur_type:=", ur_type]), value_type=str
     )
 
-    robot_state_publisher_node = Node(
+    # robot_controllers = PathJoinSubstitution(
+    #     [
+    #         FindPackageShare("ros2_control_demo_example_1"),
+    #         "config",
+    #         "rrbot_controllers.yaml",
+    #     ]
+    # )
+
+    robot_state_publisher_node_1 = Node(
+        # namespace="",
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{"robot_description": robot_description}],
     )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        # parameters=[robot_controllers],
+        output="both",
+    )
+
+    # robot_state_publisher_node_2 = Node(
+    #     namespace="robot_2",
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     parameters=[{"robot_description": robot_description}],
+    # )
 
     # tf_listener_node = Node(
     #     package="synbio_test",
@@ -36,6 +58,12 @@ def generate_launch_description():
     joint_state_publisher_gui_node = Node(
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        # arguments=["forward_position_controller", "--param-file", robot_controllers]
     )
 
     rviz_node = Node(
@@ -68,8 +96,10 @@ def generate_launch_description():
     return LaunchDescription(
         declared_arguments
         + [
+            control_node,
             joint_state_publisher_gui_node,
-            robot_state_publisher_node,
+            robot_state_publisher_node_1,
+            # robot_state_publisher_node_2,
             # tf_listener_node,
             rviz_node,
         ]
