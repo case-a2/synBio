122d4cd64d7c21c496eb7ab4b1d11b44d2106dbd
Case Ashton <caseashton2@gmail.com>
2025-07-28 18:03:25 -0500

    Renamed moveit config setup

--- synbio_moveit_config/launch/move_group.launch.py
+++ synbio_moveit_config/launch/move_group.launch.py
@@ -3,5 +3,24 @@ from moveit_configs_utils.launches import generate_move_group_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("my_robot_cell", package_name="synbio_moveit_config").to_moveit_configs()
-    return generate_move_group_launch(moveit_config)
+    moveit_config = (
+        MoveItConfigsBuilder("synbio_env", package_name="synbio_moveit_config")
+        .to_moveit_configs()
+    )
+
+    # Add trajectory_execution parameters for time parameterization
+    extra_parameters = [{
+        "trajectory_execution": {
+            "allowed_execution_duration_scaling": 1.2,
+            "allowed_goal_duration_margin": 0.5,
+            "allowed_start_tolerance": 0.01,
+            "moveit_manage_controllers": True,
+            "trajectory_duration_monitoring": True,
+            "trajectory_time_parameterization": {
+                "enabled": True,
+                "type": "IterativeParabolicTimeParameterization"
+            }
+        }
+    }]
+
+    return generate_move_group_launch(moveit_config, parameters=extra_parameters)
--- synbio_moveit_config/launch/setup_assistant.launch.py
+++ synbio_moveit_config/launch/setup_assistant.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_setup_assistant_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("my_robot_cell", package_name="synbio_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("synbio_env", package_name="synbio_moveit_config").to_moveit_configs()
     return generate_setup_assistant_launch(moveit_config)
